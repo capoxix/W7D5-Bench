aab9bbd2c7ab251c5cf42fe480b1f7b17ebdd2b2
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-03 16:39:32 -0700

    Halfway through

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
+
+export const sessionReducer = (state = {id: null}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, {id: action.currentUser.id});
+    case LOGOUT_CURRENT_USER:
+      return Object.assign({}, state, {id: null});
+    default:
+      return state;
+  }
+};
