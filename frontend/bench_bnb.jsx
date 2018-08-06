5378033007edd995899fb5a01baed2ffe6c84874
Garbo Cheng <garbocheng93@gmail.com>
2018-08-06 10:57:01 -0700

    Finish bootstrapping current user

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -9,11 +9,24 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', ()=> {
   window.login = login;
   window.logout = logout;
-  //change store to configureStore;
-  const store = configureStore();
-  window.store = store;
-  //login through console;
-  //store.dispatch(login({username:'test',password:'123456'}));
+  // const store = configureStore();
+  // window.store = store;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
+
+//login through console;
+// store.dispatch(login({username:'test',password:'123456'}));
