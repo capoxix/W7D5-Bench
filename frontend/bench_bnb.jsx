88cf0123f1327b18b2ba0ec7423d4da779103be4
Garbo Cheng <garbocheng93@gmail.com>
2018-08-06 10:41:08 -0700

    Finish login/logout/signup

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {signup, login, logout} from './util/session_api_util';
+import {signup, login, logout} from './actions/session_actions';
 import configureStore from './store/store';
 import Root from './components/root';
 
@@ -12,7 +12,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
   //change store to configureStore;
   const store = configureStore();
   window.store = store;
-
+  //login through console;
+  //store.dispatch(login({username:'test',password:'123456'}));
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,18 @@
+import GreetingContainer from './greeting/greeting_container';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginFormContainer from './login_form_container';
+import SignupFormContainer from './signup_form_container';
 
 const App = () => (
   <div>
-    <h1>Bench BnB</h1>
+    <header>
+      <h1>Bench BnB</h1>
+      <GreetingContainer />
+    </header>
+
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
 );
 
